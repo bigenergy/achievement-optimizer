168dd91742080ff7f1da1d51f0bc287df83d660a
Big_Energy <andre90094@gmail.com>
2024-05-20 06:57:38 +0300

    updated to recommended forge / new optimizations in ItemPredicate (fast matches)

--- src/main/java/com/bigenergy/achiopt/mixins/InventoryChangeTriggerMixin.java
+++ src/main/java/com/bigenergy/achiopt/mixins/InventoryChangeTriggerMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(InventoryChangeTrigger.class)
@@ -37,6 +38,11 @@ public class InventoryChangeTriggerMixin extends SimpleCriterionTrigger<Inventor
         return false;
     }
 
+    /**
+     * @author Big_Energy
+     * @reason performance fix
+     */
+    @Overwrite
     public void trigger(ServerPlayer p_43150_, Inventory p_43151_, ItemStack p_43152_) {
         if (!this.tryTick())
             return;
@@ -60,6 +66,9 @@ public class InventoryChangeTriggerMixin extends SimpleCriterionTrigger<Inventor
         this.trigger(p_43150_, p_43151_, p_43152_, i, j, k);
     }
 
+    /**
+     * @author Big_Energy
+     */
     private void trigger(ServerPlayer p_43154_, Inventory p_43155_, ItemStack p_43156_, int p_43157_, int p_43158_, int p_43159_) {
         this.trigger(p_43154_, (p_43166_) -> {
             return p_43166_.matches(p_43155_, p_43156_, p_43157_, p_43158_, p_43159_);
---
+++ src/main/java/com/bigenergy/achiopt/mixins/ItemPredicateMixin.java
@@ -0,0 +1,134 @@
+package com.bigenergy.achiopt.mixins;
+
+import net.minecraft.advancements.critereon.EnchantmentPredicate;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.NbtPredicate;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.advancements.critereon.MinMaxBounds;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.Set;
+
+@Mixin(ItemPredicate.class)
+public class ItemPredicateMixin {
+
+    @Shadow
+    public static final ItemPredicate ANY = null;
+    @Shadow
+    private final TagKey<Item> tag;
+    @Shadow
+    private final Set<Item> items;
+    @Shadow
+    private final MinMaxBounds.Ints count;
+    @Shadow
+    private final MinMaxBounds.Ints durability;
+    @Shadow
+    private final EnchantmentPredicate[] enchantments;
+    @Shadow
+    private final EnchantmentPredicate[] storedEnchantments;
+    @Shadow
+    private final Potion potion;
+    @Shadow
+    private final NbtPredicate nbt;
+
+    public ItemPredicateMixin() {
+        this.tag = null;
+        this.items = null;
+        this.potion = null;
+        this.count = MinMaxBounds.Ints.ANY;
+        this.durability = MinMaxBounds.Ints.ANY;
+        this.enchantments = EnchantmentPredicate.NONE;
+        this.storedEnchantments = EnchantmentPredicate.NONE;
+        this.nbt = NbtPredicate.ANY;
+    }
+
+    @Inject(method = "matches", at = @At("HEAD"), cancellable = true)
+    private void onMatches(ItemStack itemStack, CallbackInfoReturnable<Boolean> cir) {
+        if (this.equals(ItemPredicate.ANY)) {
+            cir.setReturnValue(true);
+            return;
+        }
+
+        if (this.tag != null && !itemStack.is(this.tag)) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        if (this.items != null && !this.items.contains(itemStack.getItem())) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        if (!this.count.matches(itemStack.getCount())) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        if (!this.durability.isAny()) {
+            if (!itemStack.isDamageableItem() || !this.durability.matches(itemStack.getMaxDamage() - itemStack.getDamageValue())) {
+                cir.setReturnValue(false);
+                return;
+            }
+        }
+
+        if (!this.nbt.matches(itemStack)) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        // Check enchantments
+        if (this.enchantments.length > 0 && !achievementOptimizer$checkEnchantments(itemStack, this.enchantments)) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        // Check stored enchantments
+        if (this.storedEnchantments.length > 0 && !achievementOptimizer$checkStoredEnchantments(itemStack, this.storedEnchantments)) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        // Check potion
+        if (this.potion != null && this.potion != PotionUtils.getPotion(itemStack)) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+        cir.setReturnValue(true);
+    }
+
+    @Unique
+    private boolean achievementOptimizer$checkEnchantments(ItemStack itemStack, EnchantmentPredicate[] enchantments) {
+        Map<Enchantment, Integer> enchantmentsMap = itemStack.getAllEnchantments();
+        for (EnchantmentPredicate predicate : enchantments) {
+            if (!predicate.containedIn(enchantmentsMap)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Unique
+    private boolean achievementOptimizer$checkStoredEnchantments(ItemStack itemStack, EnchantmentPredicate[] storedEnchantments) {
+        Map<Enchantment, Integer> storedEnchantmentsMap = EnchantmentHelper.deserializeEnchantments(EnchantedBookItem.getEnchantments(itemStack));
+        for (EnchantmentPredicate predicate : storedEnchantments) {
+            if (!predicate.containedIn(storedEnchantmentsMap)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
